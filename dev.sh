49e179669a72bef0452ddc4b1aeed14e6fd5d280
dreamoutbox <262966751+dreamoutbox@users.noreply.github.com>
2026-07-31 01:17:00 +0700

    feat(dev): update dev.sh to start cargo watch backend and pnpm frontend concurrently with signal traps

--- dev.sh
+++ dev.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-cargo watch -w src -x "run --bin rust-restaurant-system"
+# Terminate all background jobs when exiting or pressing Ctrl+C
+trap 'kill $(jobs -p) 2>/dev/null' EXIT INT TERM
+
+cargo watch -w src -x "run --bin rust-restaurant-system" &
+pnpm --prefix web dev &
+
+wait
